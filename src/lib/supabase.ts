24b14b00e9fd36c7efc72ae9a2ba60ee89c1b083
Edgar M <edgar_swiss16@hotmail.com>
2025-07-23 15:05:53 -0700

    add supabase-cli package and configure client to use local db

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,8 +2,8 @@ import { createClient } from '@supabase/supabase-js';
 import type { Database } from '@/types/database.types';
 
 // Usamos variables estáticas para el desarrollo, pero en producción usarías variables de entorno
-const supabaseUrl = 'https://qpwtknfbineefqazhmyn.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFwd3RrbmZiaW5lZWZxYXpobXluIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYyNDkyMzAsImV4cCI6MjA2MTgyNTIzMH0.z25NlHRuYsHXaye7jTbeCtNiBXlFS4g4Qg3PRhupUlU';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 // Creamos un cliente tipado solo cuando tenemos URL y KEY válidos
 let supabaseClient: ReturnType<typeof createClient<Database>> | null = null;
